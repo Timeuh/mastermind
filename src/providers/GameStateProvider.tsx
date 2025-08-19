9463188569b04f0bdccf59a564c8e3e044236bd0
Timeuh <timeuuh@gmail.com>
2025-08-19 18:17:21 +0200

    ✨ init game state and make possible to swap it

--- src/providers/GameStateProvider.tsx
+++ src/providers/GameStateProvider.tsx
@@ -1,17 +1,38 @@
-import React, {useContext, type ReactNode} from 'react';
+import {createContext, useContext, useState, type ReactNode} from 'react';
+import type {GameState, GameStateContext} from '../types/app_types';
 
 type Props = {
   children: ReactNode;
 };
 
-const GameStateContext = React.createContext({});
+// create the game state context as undefined first
+const GSContext = createContext<GameStateContext | undefined>(undefined);
 
+/**
+ * GameStateProvider component to provide game state context
+ *
+ * @param children - children components which can access the context
+ */
 export default function GameStateProvider({children}: Props) {
-  return <GameStateContext.Provider value={{}}>{children}</GameStateContext.Provider>;
+  const [gameState, setGameState] = useState<GameState>('HOME');
+
+  /**
+   * Function to swap the current game state
+   *
+   * @param state - The new game state to set.
+   */
+  const swapState = (state: GameState) => {
+    setGameState(state);
+  };
+
+  return <GSContext.Provider value={{gameState, swapState}}>{children}</GSContext.Provider>;
 }
 
-export const useGameState = () => {
-  const context = useContext(GameStateContext);
+/**
+ * Custom hook to access the game state context.
+ */
+export const useGameState = (): GameStateContext => {
+  const context: GameStateContext | undefined = useContext(GSContext);
   if (!context) {
     throw new Error('useGameState must be used within a GameStateProvider');
   }
