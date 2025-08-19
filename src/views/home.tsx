fbf95b939f071d48b735b3c73fdfbb8d1e8e5799
Timeuh <timeuuh@gmail.com>
2025-08-19 17:09:48 +0200

    💄 style the page with the title and background

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className='font-main'>
-      <h1 className='text-4xl'>Mastermind</h1>
-    </main>
+    <section className='bg-app-bg flex h-screen w-full flex-col items-center justify-center'>
+      <h1 className='text-app-cta text-shadow-app-main text-7xl font-bold text-shadow-md'>MASTERMIND</h1>
+    </section>
   );
 }
