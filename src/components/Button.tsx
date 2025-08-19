b9d1ee7a368426d74bd645359de620e20dcb4ab0
Timeuh <timeuuh@gmail.com>
2025-08-19 18:26:39 +0200

    ✨ add onClick event handling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,14 @@
 type Props = {
   text: string;
+  onClick: () => void;
 };
 
-export default function Button({text}: Props) {
+export default function Button({text, onClick}: Props) {
   return (
-    <button className='bg-app-cta text-app-darker shadow-app-darker/50 hover:shadow-app-main/40 w-[15vw] rounded py-1 text-2xl font-bold shadow-xl transition duration-500 ease-in-out hover:cursor-pointer'>
+    <button
+      onClick={onClick}
+      className='bg-app-cta text-app-darker shadow-app-darker/50 hover:shadow-app-main/40 w-[15vw] rounded py-1 text-2xl font-bold shadow-xl transition duration-500 ease-in-out hover:cursor-pointer'
+    >
       {text}
     </button>
   );
