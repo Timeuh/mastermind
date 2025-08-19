ac9328628e7c3b35364c56d378266702acdfb46d
Timeuh <timeuuh@gmail.com>
2025-08-19 17:23:01 +0200

    💄 increase space between title and button

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,7 +2,7 @@ import Button from '../components/Button';
 
 export default function Home() {
   return (
-    <section className='bg-app-bg flex h-screen w-full flex-col items-center justify-center space-y-10'>
+    <section className='bg-app-bg flex h-screen w-full flex-col items-center justify-center space-y-[6vh]'>
       <h1 className='text-app-cta text-shadow-app-main text-7xl font-bold text-shadow-md'>MASTERMIND</h1>
       <Button text='Jouer' />
     </section>
