2e1b8b97b11aaa2a94f3ed3e3e90dc42a5e91f64
Timeuh <timeuuh@gmail.com>
2025-08-19 17:56:10 +0200

    ✨ display home circles decoration

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,18 @@
 import Button from '../components/Button';
+import Circle from '../components/Circle';
+import {home_circle} from '../data/home_circles';
+import type {HomeCircle} from '../types/app_types';
 
 export default function Home() {
   return (
     <section className='bg-app-bg flex h-screen w-full flex-col items-center justify-center space-y-[6vh]'>
       <h1 className='text-app-cta text-shadow-app-main text-7xl font-bold text-shadow-md'>MASTERMIND</h1>
       <Button text='Jouer' />
+      <div>
+        {home_circle.map((circle: HomeCircle, index: number) => (
+          <Circle key={index} color={circle.color} position={circle.position} absolutePosition />
+        ))}
+      </div>
     </section>
   );
 }
