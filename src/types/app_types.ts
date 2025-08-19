dcc39af743243c9e31be9f625d2b3a0b1c1647f0
Timeuh <timeuuh@gmail.com>
2025-08-19 18:16:55 +0200

    🏷️ add types relative to game state and state context

--- src/types/app_types.ts
+++ src/types/app_types.ts
@@ -3,3 +3,12 @@ export type HomeCircle = {
   color: string;
   position: string;
 };
+
+// possible game states
+export type GameState = 'HOME' | 'CONFIG' | 'PLAYING';
+
+// context for game state
+export type GameStateContext = {
+  gameState: GameState;
+  swapState: (state: GameState) => void;
+};
