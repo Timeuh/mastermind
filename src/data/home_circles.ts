6b9ac2c1b80b7317ed0a1173b1ac7541cd18e7eb
Timeuh <timeuuh@gmail.com>
2025-08-19 17:56:00 +0200

    ✨ create home circles array data

---
+++ src/data/home_circles.ts
@@ -0,0 +1,72 @@
+import type {HomeCircle} from '../types/app_types';
+
+export const home_circle: HomeCircle[] = [
+  {
+    color: 'bg-game-red',
+    position: 'top-[5vh] left-[5vw]',
+  },
+  {
+    color: 'bg-game-orange',
+    position: 'top-[6vh] left-[30vw]',
+  },
+  {
+    color: 'bg-game-green',
+    position: 'top-[5vh] right-[30vw]',
+  },
+  {
+    color: 'bg-game-blue',
+    position: 'top-[4vh] right-[5vw]',
+  },
+  {
+    color: 'bg-game-pink',
+    position: 'top-[35vh] left-[7vw]',
+  },
+  {
+    color: 'bg-game-purple',
+    position: 'top-[25vh] left-[20vw]',
+  },
+  {
+    color: 'bg-game-yellow',
+    position: 'top-[20vh] left-[45vw]',
+  },
+  {
+    color: 'bg-game-orange',
+    position: 'top-[25vh] right-[20vw]',
+  },
+  {
+    color: 'bg-game-yellow',
+    position: 'bottom-[25vh] left-[5vw]',
+  },
+  {
+    color: 'bg-game-orange',
+    position: 'bottom-[40vh] left-[25vw]',
+  },
+  {
+    color: 'bg-game-blue',
+    position: 'bottom-[25vh] left-[45vw]',
+  },
+  {
+    color: 'bg-game-red',
+    position: 'bottom-[35vh] right-[25vw]',
+  },
+  {
+    color: 'bg-game-pink',
+    position: 'bottom-[45vh] right-[10vw]',
+  },
+  {
+    color: 'bg-game-green',
+    position: 'bottom-[10vh] left-[15vw]',
+  },
+  {
+    color: 'bg-game-red',
+    position: 'bottom-[5vh] left-[35vw]',
+  },
+  {
+    color: 'bg-game-purple',
+    position: 'bottom-[10vh] right-[35vw]',
+  },
+  {
+    color: 'bg-game-orange',
+    position: 'bottom-[15vh] right-[10vw]',
+  },
+];
