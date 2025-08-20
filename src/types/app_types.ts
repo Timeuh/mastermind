a9926b1b82b462470d6e1bc9000ee03fbe034c3b
Timeuh <timeuuh@gmail.com>
2025-08-20 14:10:05 +0200

    🏷️ add types related to game configuration and game conf context

--- src/types/app_types.ts
+++ src/types/app_types.ts
@@ -12,3 +12,27 @@ export type GameStateContext = {
   gameState: GameState;
   swapState: (state: GameState) => void;
 };
+
+// number of colors to guess
+export type ColorNumber = 4 | 5 | 6;
+
+// setting for answer indication when guessing
+export type AnswerIndicator = 'PARTIAL' | 'FULL';
+
+// setting for maximum attempts at guessing
+export type MaxAttempts = 'TEN' | 'INFINITE';
+
+// configuration for the game
+export type GameConfig = {
+  colorNumber: ColorNumber;
+  answerIndicator: AnswerIndicator;
+  maxAttempts: MaxAttempts;
+};
+
+// context for game configuration
+export type GameConfigContext = {
+  gameConfig: GameConfig;
+  chooseColorNumber: (colorNumber: ColorNumber) => void;
+  chooseAnswerIndicator: (answerIndicator: AnswerIndicator) => void;
+  chooseMaxAttempts: (maxAttempts: MaxAttempts) => void;
+};
