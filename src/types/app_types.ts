791f2db4c6da740a4dfbad911e914a27375b7ca5
Timeuh <timeuuh@gmail.com>
2025-08-21 14:02:39 +0200

    🏷️ add types to play the game

--- src/types/app_types.ts
+++ src/types/app_types.ts
@@ -36,3 +36,54 @@ export type GameConfigContext = {
   chooseAnswerIndicator: (answerIndicator: AnswerIndicator) => void;
   chooseMaxAttempts: (maxAttempts: MaxAttempts) => void;
 };
+
+// game possible colors
+export type GameColor = 'RED' | 'ORANGE' | 'YELLOW' | 'GREEN' | 'BLUE' | 'PURPLE' | 'PINK' | 'BROWN' | 'WHITE';
+
+// possible statuses for a color in an answer
+export type AnswerStatus = 'CORRECT' | 'INCORRECT' | 'PARTIAL';
+
+// possible row lengths
+export type Row =
+  | [GameColor, GameColor, GameColor, GameColor]
+  | [GameColor, GameColor, GameColor, GameColor, GameColor]
+  | [GameColor, GameColor, GameColor, GameColor, GameColor, GameColor];
+
+// possible answer row lengths
+export type AnswerRow =
+  | [AnswerStatus, AnswerStatus, AnswerStatus, AnswerStatus]
+  | [AnswerStatus, AnswerStatus, AnswerStatus, AnswerStatus, AnswerStatus]
+  | [AnswerStatus, AnswerStatus, AnswerStatus, AnswerStatus, AnswerStatus, AnswerStatus];
+
+// game guess with associated answer
+export type Guess<T extends ColorNumber> = {
+  guess: Row[T];
+  answer: AnswerRow[T];
+};
+
+// current game data
+export type Game<T extends ColorNumber> = {
+  toGuess: Row[T];
+  currentGuess: Row[T];
+  ancientGuesses: Guess<T>[];
+};
+
+// map game colors to tailwind background colors
+export const gameColorToBackground: Record<GameColor, string> = {
+  RED: 'bg-game-red',
+  ORANGE: 'bg-game-orange',
+  YELLOW: 'bg-game-yellow',
+  GREEN: 'bg-game-green',
+  BLUE: 'bg-game-blue',
+  PURPLE: 'bg-game-purple',
+  PINK: 'bg-game-pink',
+  BROWN: 'bg-game-brown',
+  WHITE: 'bg-app-text',
+};
+
+// map answer statuses to tailwind background colors
+export const answerStatusToBackground: Record<AnswerStatus, string> = {
+  CORRECT: 'bg-app-main',
+  INCORRECT: 'bg-app-cta',
+  PARTIAL: 'bg-app-dark',
+};
