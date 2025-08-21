13f99805ba4da4993527ebafbbf7f77a158970e8
Timeuh <timeuuh@gmail.com>
2025-08-21 15:30:34 +0200

    ✨ create function to select the answer

---
+++ src/functions/choose_answer.ts
@@ -0,0 +1,23 @@
+import type {ColorNumber, GameColor, Row} from '../types/app_types';
+
+/**
+ * Randomly selects colors for the answer
+ *
+ * @param colorNumber {ColorNumber} - The number of colors to select
+ */
+export const chooseAnswer = (colorNumber: ColorNumber): Row => {
+  // possible colors to pick
+  const GAME_COLORS: GameColor[] = ['RED', 'ORANGE', 'YELLOW', 'GREEN', 'BLUE', 'PURPLE', 'PINK', 'BROWN'];
+
+  // chosen colors for the answer
+  const answer: GameColor[] = [];
+
+  // randomly select colors for the answer
+  for (let i = 0; i < colorNumber; i++) {
+    const index = Math.floor(Math.random() * GAME_COLORS.length);
+    answer.push(GAME_COLORS[index]);
+  }
+
+  // return the selected answer
+  return answer as Row;
+};
