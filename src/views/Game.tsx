387e98eb13fdcf1e9093fd117c93e3ea90e4c8c4
Timeuh <timeuuh@gmail.com>
2025-08-21 16:14:27 +0200

    ✨ create game color selector, validator and help

--- src/views/Game.tsx
+++ src/views/Game.tsx
@@ -1,11 +1,21 @@
+import {useState} from 'react';
 import ColorSelector from '../components/ColorSelector';
 import Help from '../components/Help';
 import {useGameConfig} from '../providers/GameConfProvider';
 import {useGameState} from '../providers/GameStateProvider';
+import type {ColorNumber, Game} from '../types/app_types';
+import {initGame} from '../functions/init_game';
+import Button from '../components/Button';
 
 export default function Game() {
   const {swapState} = useGameState();
   const {gameConfig} = useGameConfig();
+
+  // get the number of colors to guess in the game
+  const colorNumber = gameConfig.colorNumber;
+
+  // initialize blank game
+  const [_game, _setGame] = useState<Game<ColorNumber>>(() => initGame(colorNumber));
 
   // go to home page
   const goBack = () => {
@@ -20,8 +30,17 @@ export default function Game() {
       >
         MASTERMIND
       </h1>
-      <ColorSelector />
-      <Help answerIndicator={gameConfig.answerIndicator} />
+      <div className='flex w-full flex-row items-center space-x-6 px-[5vw]'>
+        <div className='flex flex-col items-center space-y-6'>
+          <ColorSelector />
+          <Button onClick={() => {}} text='Valider' />
+          <Help answerIndicator={gameConfig.answerIndicator} />
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </section>
   );
 }
