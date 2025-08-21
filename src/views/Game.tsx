2e7a19401f5b823c0d01f4bc3914601ba9aff58f
Timeuh <timeuuh@gmail.com>
2025-08-21 14:09:02 +0200

    ✨ make possible to go back to home by clicking title

--- src/views/Game.tsx
+++ src/views/Game.tsx
@@ -1,8 +1,21 @@
+import {useGameState} from '../providers/GameStateProvider';
+
 export default function Game() {
+  const {swapState} = useGameState();
+
+  // go to home page
+  const goBack = () => {
+    swapState('HOME');
+  };
+
   return (
     <section className='bg-app-bg flex h-screen w-full flex-col items-center justify-center space-y-[6vh]'>
-      <h1 className='text-app-cta text-shadow-app-main text-7xl font-bold text-shadow-md'>MASTERMIND</h1>
-      <h2>VIEW PLAYING</h2>
+      <h1
+        onClick={goBack}
+        className='text-app-cta text-shadow-app-main text-7xl font-bold text-shadow-md hover:cursor-pointer'
+      >
+        MASTERMIND
+      </h1>
     </section>
   );
 }
