70830844b18e64490bf0e2fcb6a5ac1f74ad2749
Timeuh <timeuuh@gmail.com>
2025-08-23 16:07:24 +0200

    ✨ handle guess submission

--- src/components/GameInterface.tsx
+++ src/components/GameInterface.tsx
@@ -1,10 +1,14 @@
-import type {AnswerIndicator} from '../types/app_types';
+import type {Dispatch, SetStateAction} from 'react';
+import {useCurrentGuessRow} from '../providers/CurrentGuessRowProvider';
+import type {AnswerIndicator, ColorNumber, Game} from '../types/app_types';
 import Button from './Button';
 import ColorSelector from './ColorSelector';
 import Help from './Help';
+import {createGuess} from '../functions/create_guess';
 
 type Props = {
   answerIndicator: AnswerIndicator;
+  setGame: Dispatch<SetStateAction<Game<ColorNumber>>>;
 };
 
 /**
@@ -12,11 +16,30 @@ type Props = {
  *
  * @param answerIndicator {AnswerIndicator} - the type of answer indicator for the help
  */
-export default function GameInterface({answerIndicator}: Props) {
+export default function GameInterface({answerIndicator, setGame}: Props) {
+  const {currentGuessRow, checkCanGuess, resetCurrentGuess} = useCurrentGuessRow();
+
+  /**
+   * Handle the guess submission
+   */
+  const handleGuess = () => {
+    // if the answer row isnt fully filled
+    if (!checkCanGuess()) return;
+
+    // register the guessed row
+    setGame((prevGame: Game<ColorNumber>) => ({
+      ...prevGame,
+      ancientGuesses: [...prevGame.ancientGuesses, createGuess<ColorNumber>(currentGuessRow)],
+    }));
+
+    // reset the current guess
+    resetCurrentGuess();
+  };
+
   return (
     <div className='flex flex-col items-center space-y-6'>
       <ColorSelector />
-      <Button onClick={() => {}} text='Valider' />
+      <Button onClick={handleGuess} text='Valider' />
       <Help answerIndicator={answerIndicator} />
     </div>
   );
