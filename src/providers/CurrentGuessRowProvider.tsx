6092ad327578f59e7af2f7edf66308d6ec1b272d
Timeuh <timeuuh@gmail.com>
2025-08-23 15:18:59 +0200

    ✨ create current guess row context with utilities functions

--- src/providers/CurrentGuessRowProvider.tsx
+++ src/providers/CurrentGuessRowProvider.tsx
@@ -1,26 +1,60 @@
-import {createContext, useContext, type ReactNode} from 'react';
+import {createContext, useContext, useState, type ReactNode} from 'react';
+import type {CurrentGuessRowContext, GameColor} from '../types/app_types';
 
 type Props = {
   children: ReactNode;
+  circleNumber: number;
 };
 
 // create the current guess row context as undefined first
-const CGRContext = createContext<undefined>(undefined);
+const CGRContext = createContext<CurrentGuessRowContext | undefined>(undefined);
 
 /**
  * CurrentGuessRowProvider component to provide current guess row context
  *
  * @param children - children components which can access the context
  */
-export default function CurrentGuessRowProvider({children}: Props) {
-  return <CGRContext.Provider value={undefined}>{children}</CGRContext.Provider>;
+export default function CurrentGuessRowProvider({children, circleNumber}: Props) {
+  const [currentGuessRow, setCurrentGuessRow] = useState<GameColor[]>(Array(circleNumber).fill('WHITE'));
+
+  /**
+   * Check if the current guess row is complete before guessing
+   */
+  const checkCanGuess = (): boolean => {
+    return currentGuessRow.every((color: GameColor) => color !== 'WHITE');
+  };
+
+  /**
+   * Reset the current guess row to the initial state
+   */
+  const resetCurrentGuess = () => {
+    setCurrentGuessRow(Array(circleNumber).fill('WHITE'));
+  };
+
+  /**
+   * Change the color of a specific circle in the current guess row
+   *
+   * @param index {number} - The index of the circle to change
+   * @param color {GameColor} - The new color for the circle
+   */
+  const changeCircleColor = (index: number, color: GameColor) => {
+    setCurrentGuessRow((prev: GameColor[]) =>
+      prev.map((previousColor: GameColor, currentIndex: number) => (index === currentIndex ? color : previousColor)),
+    );
+  };
+
+  return (
+    <CGRContext.Provider value={{currentGuessRow, checkCanGuess, resetCurrentGuess, changeCircleColor}}>
+      {children}
+    </CGRContext.Provider>
+  );
 }
 
 /**
  * Custom hook to access the current guess row context.
  */
-export const useCurrentGuessRow = () => {
-  const context = useContext<undefined>(CGRContext);
+export const useCurrentGuessRow = (): CurrentGuessRowContext => {
+  const context = useContext<CurrentGuessRowContext | undefined>(CGRContext);
   if (!context) {
     throw new Error('useCurrentGuessRow must be used within a CurrentGuessRowProvider');
   }
